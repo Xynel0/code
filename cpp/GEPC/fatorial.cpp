857267b2cb4a7b6c083067c755bf5ea53b30ddb5
Xinelo <261785386+Xynel0@users.noreply.github.com>
2026-04-04 11:12:27 -0300

    Update fatorial.cpp

--- cpp/GEPC/fatorial.cpp
+++ cpp/GEPC/fatorial.cpp
@@ -2,19 +2,23 @@
 using namespace std;
 int main(){
     int n;
+
+    //recebe n
     cout << "insira n:" << endl;
     cin >> n;
-        if (n > 0 && n <= 12){
-            for (int i = n - 1; i > 0; i--)
-            {
-                n = n*i;
-            }
-            cout << n << endl;
+    //se n for maior que 0 e estiver dentro do intervalo válido...
+    if (n > 0 && n <= 12){
+        //realiza o fatorial
+        for (int i = n - 1; i > 0; i--){
+            n = n*i;
         }
-        else if (n == 0){
-            cout << "1" << endl;
-        }
-        else{
-            cout << "entrada inválida" << endl;
-        }
+        cout << n << endl; //imprime o resultado
+    }
+    //se n for 0, imprime 1
+    else if (n == 0){
+        cout << "1" << endl;
+    }
+    else{
+        cout << "entrada inválida" << endl;
+    }
 }

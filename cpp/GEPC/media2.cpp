4a75539940e432a286b6fd2fb96524936c4d53fa
Xinelo <261785386+Xynel0@users.noreply.github.com>
2026-04-04 11:22:53 -0300

    Create media2.cpp

---
+++ cpp/GEPC/media2.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    double a;
+    double b;
+    double c;
+    double media;
+    cin >> a;
+    cin >> b;
+    cin >> c;
+    media = (a*2 + b*3 + c*5) / 10;
+    cout << "MEDIA = " << fixed << setprecision(1) << media << endl;
+}

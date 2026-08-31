d755e6dc9632a2e60cd5dcb901223884b1dac77b
Xinelo Kenner dy Olympikus <261785386+Xynel0@users.noreply.github.com>
2026-08-31 15:45:43 -0300

    Create imc.kt

---
+++ kotlin/pdm/imc.kt
@@ -0,0 +1,15 @@
+import java.time.LocalDate
+import java.time.format.TextStyle
+import java.util.Locale
+
+fun imc(args: Array<String>): String {
+    var peso = args[0].toDouble()
+    var altura = args[1].toDouble()
+    var imcRes = peso/(altura*altura)
+    return "%.2f".format(imcRes)
+}
+
+fun main(args: Array<String>) {    
+    var imcResult = imc(args)
+    println("O seu imc é $imcResult")
+}

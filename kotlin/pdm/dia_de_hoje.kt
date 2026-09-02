f41d6224aab9febee0c79f5e559c33d2ad3fb73b
Xinelo Kenner dy Olympikus <261785386+Xynel0@users.noreply.github.com>
2026-09-01 23:18:19 -0300

    Atualizar o dia_de_hoje.kt

--- kotlin/pdm/dia_de_hoje.kt
+++ kotlin/pdm/dia_de_hoje.kt
@@ -3,9 +3,9 @@ import java.time.format.TextStyle
 import java.util.Locale
 
 fun main() {
-   var today = LocalDate.now()
-	  var localeBR = Locale("pt", "BR")
-   var weekday = today.dayOfWeek.getDisplayName(TextStyle.FULL, localeBR)
+    var today = LocalDate.now()
+	   var localeBR = Locale("pt", "BR")
+    var weekday = today.dayOfWeek.getDisplayName(TextStyle.FULL, localeBR)
     var day = today.dayOfMonth
     var month = today.month.getDisplayName(TextStyle.FULL, localeBR)
     println("hoje é $weekday, dia $day de $month!")

1afac2b114572e6a424a98612e71f404b3773872
Xinelo Kenner dy Olympikus <261785386+Xynel0@users.noreply.github.com>
2026-09-23 23:21:16 -0300

    Create encontrar_elemento_mais_proximo.c

---
+++ C/programacao/programando_com_listas_encadeadas/encontrar_elemento_mais_proximo.c
@@ -0,0 +1,43 @@
+// inclua as bibliotecas e definas as variáveis globais de entrada e saída do modelo
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+typedef struct NoLista{
+	int valor;
+	struct NoLista * prox;
+} NoLista;
+
+NoLista * criar_no(int valor, NoLista * prox){
+	NoLista * no = malloc(sizeof(NoLista));
+	no->valor = valor;
+	no->prox = prox;
+	return no;
+}
+
+NoLista * p;
+
+int k = 7;
+
+int resp;
+
+/*
+a inicialização será feita no sistema de tarefas
+p = criar_no(5, criar_no(8, criar_no(13, criar_no(2, NULL))));
+*/
+
+
+// -- escreva seu código abaixo, não altere esta linha
+
+
+
+int main() {
+    resp = p->valor;
+    while(p->prox != NULL){
+        if(abs(p->valor - k) < abs(resp - k)){
+            resp = p->valor;
+        }
+        p = p->prox;
+    }
+    printf("%d\n", resp);
+}

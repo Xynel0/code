7dbc3501eb12a7edf2578b3b91638c780fa1e52c
Xinelo Kenner dy Olympikus <261785386+Xynel0@users.noreply.github.com>
2026-09-26 17:36:17 -0300

    Create mover_ultimo_para_o_inicio.c

---
+++ C/programacao/manipulando_listas_encadeadas/mover_ultimo_para_o_inicio.c
@@ -0,0 +1,41 @@
+// inclua as bibliotecas e definas as variáveis globais de entrada e saída do modelo
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+typedef struct NoLista{
+	int valor;
+	struct NoLista * prox;
+} NoLista;
+
+NoLista * criar_no(int valor, NoLista * prox){
+	NoLista * no = malloc(sizeof(NoLista));
+	no->valor = valor;
+	no->prox = prox;
+	return no;
+}
+
+NoLista * p;
+
+
+/*
+a inicialização será feita no sistema de tarefas
+p = criar_no(5, criar_no(8, criar_no(13, criar_no(2, NULL))));
+*/
+
+
+// -- escreva seu código abaixo, não altere esta linha
+
+
+
+int main() {
+    NoLista * pointer = p;
+    if(pointer->prox != NULL){
+        while (pointer->prox->prox != NULL){
+            pointer = pointer->prox;
+        }
+        pointer->prox->prox = p;
+        p = pointer->prox;
+        pointer->prox = NULL;
+    }
+}
